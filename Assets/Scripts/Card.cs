61e672b9899a3de209c0a5ea4b3279f31f0ff86a
Fernando Cortez <fernando.alberto.cortez@gmail.com>
2020-09-26 19:23:39 -0400

    card class created with suits & ranks enums and deck class created (implements a stack)

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using UnityEngine;
+
+namespace Blackjack
+{
+    public enum Suit : byte
+    {
+        Club = 0,
+        Diamond = 1,
+        Heart = 2,
+        Spade = 3,
+        COUNT = 4
+    }
+
+    public enum Rank : byte
+    {
+        Ace = 0,
+        Two = 1,
+        Three = 2,
+        Four = 3,
+        Five = 4,
+        Six = 5,
+        Seven = 6,
+        Eight = 7,
+        Nine = 8,
+        Ten = 9,
+        Jack = 10,
+        Queen = 11,
+        King = 12,
+        COUNT = 13
+    }
+
+    public class Card : MonoBehaviour
+    {
+        public CardData cardData;
+    }
+
+    [Serializable]
+    public struct CardData
+    {
+        public Suit suit;
+        public Rank rank;
+
+        public bool isHidden;
+
+        public CardData(Suit suit, Rank rank, bool isHidden)
+        {
+            this.suit = suit;
+            this.rank = rank;
+            this.isHidden = isHidden;
+        }
+    }
+}
---
+++ Assets/Scripts/Deck.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Blackjack
+{
+    public class Deck : MonoBehaviour
+    {
+        private Stack<CardData> cards;
+
+
+    }
+}
